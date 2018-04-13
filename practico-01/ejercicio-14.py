f7833438111894233ed5ac9292d683e74074e7c6
Ale Suárez <alepersa087@gmail.com>
2018-04-13 16:17:47 -0300

    practica 1 -ej 14
    modificacion de ejercicio 14

--- practico-01/ejercicio-14.py
+++ practico-01/ejercicio-14.py
@@ -1,34 +1,105 @@
-from random import randint
+a=[['','x','x','',''],
+   ['','','','x' ,  ''],
+   ['' ,'x','x','x',"salida"],
+   ['' ,'' ,'','','']]
 
-fila = randint(4, 10)
-col = 2
+posicionIni=[0,0]
+posicionUlt=[0,0]
+bandera=0
 
-laberinto = [range(fila) for i in range(col)] #defino al laberinto como una matrix de 2 columnas y X filas (entre 4 y 10)
-
-i=0
-while i<fila: #lleno los valores del laberinto con pares True False con una funcion random y de ese resulta, obtengo el valor contrario para l aotra columna
-    if 5>randint(1, 10):
-        laberinto[i][0] = True
-    else:
-        laberinto[i][0] = False
-    if laberinto[i][0]:
-        laberinto[i][1] = False
-    else:
-        laberinto[i][1] = False
-
-i = 0
-while i<fila:
-    if laberinto[i][0] == False:
-        print("Camino: ", i, 0)
-        i+=1
-    else:
-        print("Camino: ", i, 1)
-        i+=1
+def resolver_laberinto(laberinto,posicion,ultimaPosicion,bandera):
+    while bandera!=1:
+        #derecha
+        if (posicion[1])<(len(laberinto[1])-1):
+            if (posicion[1]+1)!= ultimaPosicion[1]:
+                if laberinto[posicion[0]][posicion[1]+1]=='':
+                    ultimaPosicion[0]=posicion[0]
+                    ultimaPosicion[1]=posicion[1]
+                    posicion=[posicion[0],posicion[1]+1]
+                    print(posicion)
+                    resolver_laberinto(laberinto,posicion,ultimaPosicion,bandera)
+                elif (laberinto[posicion[0]][posicion[1]+1]=="salida"):
+                    print("ganaste")
+                    bandera=1
 
 
+        #abajo
+        if (posicion[0]) < (len(laberinto)-1):
+            if (posicion[0]+1)!= ultimaPosicion[0]:
+                if laberinto[posicion[0]+1][posicion[1]]=='':
+                    ultimaPosicion[0]=posicion[0]
+                    ultimaPosicion[1]=posicion[1]
+                    posicion=[posicion[0]+1,posicion[1]]
+                    print(posicion)
+                    resolver_laberinto(laberinto,posicion,ultimaPosicion,bandera)
+                elif (laberinto[posicion[0]+1][posicion[1]]=="salida"):
+                    print("ganaste")
+                    bandera=1
+
+        #arriba
+        if posicion[0]>0:
+            if (posicion[0]-1)!= ultimaPosicion[0]:
+               if laberinto[posicion[0]-1][posicion[1]] == '':
+                   ultimaPosicion[0]=posicion[0]
+                   ultimaPosicion[1]=posicion[1]
+                   posicion=[posicion[0]-1,posicion[1]]
+                   print(posicion)
+                   resolver_laberinto(laberinto,posicion,ultimaPosicion,bandera)
+               elif (laberinto[posicion[0]-1][posicion[1]]=="salida"):
+                   print("ganaste")
+                   bandera=1
+
+        #izquierda
+        if (posicion[1])>0:
+            if (posicion[1]-1)!= ultimaPosicion[1]:
+                if laberinto[posicion[0]][posicion[1]-1]=='':
+                    ultimaPosicion[0]=posicion[0]
+                    ultimaPosicion[1]=posicion[1]
+                    posicion=[posicion[0],posicion[1]-1]
+                    print(posicion)
+                    resolver_laberinto(laberinto,posicion,ultimaPosicion,bandera)
+                elif (laberinto[posicion[0]][posicion[1]-1]=="salida"):
+                    print("ganaste")
+                    bandera=1
+        #encerrado abajo derecha y arriba
+        if ((laberinto[posicion[0]+1][posicion[1]]=='x' or posicion[0]+1==len(laberinto)-1) and laberinto[posicion[0]][posicion[1]+1]=='x' and laberinto[posicion[0]-1][posicion[1]]=='x' ):
+            if (posicion[1]-1) == ultimaPosicion[1]:
+                 ultimaPosicion[0]=posicion[0]
+                 ultimaPosicion[1]=posicion[1]
+                 posicion=[posicion[0],posicion[1]-1]
+                 print(posicion)
+                 resolver_laberinto(laberinto,posicion,ultimaPosicion,bandera)
+
+        #encerrado abajo derecha y izquierda
+        if (laberinto[posicion[0]+1][posicion[1]]=='x' and laberinto[posicion[0]][posicion[1]+1]=='x' and laberinto[posicion[0]][posicion[1]-1]=='x'):
+            if (posicion[0]-1) == ultimaPosicion[1]:
+                 ultimaPosicion[0]=posicion[0]
+                 ultimaPosicion[1]=posicion[1]
+                 posicion=[posicion[0],posicion[1]-1]
+                 print(posicion)
+                 resolver_laberinto(laberinto,posicion,ultimaPosicion,bandera)
+
+        #encerrado abajo izquierda y arriba
+        if (laberinto[posicion[0]+1][posicion[1]]=='x' and laberinto[posicion[0]][posicion[1]-1]=='x' and laberinto[posicion[0]-1][posicion[1]]=='x'):
+            if (posicion[1]+1) == ultimaPosicion[1]:
+                 ultimaPosicion[0]=posicion[0]
+                 ultimaPosicion[1]=posicion[1]
+                 posicion=[posicion[0],posicion[1]-1]
+                 print(posicion)
+                 resolver_laberinto(laberinto,posicion,ultimaPosicion,bandera)
+
+        #encerrado izquierda derecha y arriba
+        if ((posicion[0]) < (len(laberinto)-1) and posicion[0]>0):
+            if (laberinto[posicion[0]][posicion[1]-1]=='x' and laberinto[posicion[0]][posicion[1]+1]=='x' and laberinto[posicion[0]-1][posicion[1]]=='x'):
+                if (posicion[0]+1) == ultimaPosicion[1]:
+                    ultimaPosicion[0]=posicion[0]
+                    ultimaPosicion[1]=posicion[1]
+                    posicion=[posicion[0],posicion[1]-1]
+                    print(posicion)
+                    resolver_laberinto(laberinto,posicion,ultimaPosicion,bandera)
 
 
 
 
 
-
+resolver_laberinto(a,posicionIni,posicionUlt,bandera)
